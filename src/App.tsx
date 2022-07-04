d13aaa0d723747054221bb1ba5c4a89caa1f0223
Gleb Glushchenko <darkvee1n@ya.ru>
2022-07-04 12:46:48 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./scss/app.scss";
 
-function App() {
+import logo from "./assets/img/logo.svg";
+import cart from "./assets/img/cart-icon.svg";
+import profile from "./assets/img/person.svg";
+import favorites from "./assets/img/heart-icon.svg";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <div className="header__content-left">
+          <a href="#" className="header__logo">
+            <img src={logo} alt="Logo" />
+          </a>
+          <div className="header__info">
+            <h3 className="header__title">REACT SNEAKERS</h3>
+            <p className="header__sub-title">Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="header__content-left">
+          <li className="header__nav-cart">
+            <a href="#">
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M7.54548 18.1818C7.99735 18.1818 8.36366 17.8155 8.36366 17.3636C8.36366 16.9118 7.99735 16.5455 7.54548 16.5455C7.09361 16.5455 6.72729 16.9118 6.72729 17.3636C6.72729 17.8155 7.09361 18.1818 7.54548 18.1818Z"
+                  stroke="#9B9B9B"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M16.5455 18.1818C16.9973 18.1818 17.3637 17.8155 17.3637 17.3636C17.3637 16.9118 16.9973 16.5455 16.5455 16.5455C16.0936 16.5455 15.7273 16.9118 15.7273 17.3636C15.7273 17.8155 16.0936 18.1818 16.5455 18.1818Z"
+                  stroke="#9B9B9B"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M1 1H4.27273L6.46545 11.9555C6.54027 12.3321 6.7452 12.6705 7.04436 12.9113C7.34351 13.1522 7.71784 13.2801 8.10182 13.2727H16.0545C16.4385 13.2801 16.8129 13.1522 17.112 12.9113C17.4112 12.6705 17.6161 12.3321 17.6909 11.9555L19 5.09091H5.09091"
+                  stroke="#9B9B9B"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </a>
+            <p>1205 руб.</p>
+          </li>
+          <li className="header__nav-favorites">
+            <a href="#">
+              <img src={favorites} alt="Profile-logo" />
+            </a>
+          </li>
+          <li className="header__nav-profile">
+            <a href="#">
+              <img src={profile} alt="Profile-logo" />
+            </a>
+          </li>
+        </ul>
       </header>
     </div>
   );
-}
+};
 
 export default App;
